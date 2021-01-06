6357422d3b1bdfa49f24faa37970333642660dc0
jerome <jerome.dupret@devinci.fr>
2021-01-06 19:29:13 +0100

    Added my files

---
+++ build_program.sh
@@ -0,0 +1,2 @@
+gcc main.c -o enumerate_numbers
+chmod u+x enumerate_numbers
---
+++ main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]){
+
+	//if (isdigit(argv[1][0]) == 0) {
+	//	printf("Hello\n");
+	//}
+
+
+	if(argc > 2){
+
+		argc = atoi(argv[1]);
+
+		for (int i = 0; i <= argc; i++) {
+			printf("%d\n", i);
+		}
+		return 0;
+
+	}else	if(argc == 2){
+		argc = atoi(argv[1]);int i = 0;
+
+		if( isalpha(argv[1][0]) != 0 || argc == 0){
+			return 0;
+
+		}
+
+		do{
+			printf("%d\n", i);
+			i++;
+
+		}while(i <= argc);
+		return 0;
+
+	}else if(argc == 1){
+
+		for (int i = 0; i <= 10; i++) {
+			printf("%d\n", i);
+		}
+		return 0;
+	}
+
+}
